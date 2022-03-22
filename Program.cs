0a148005c734de54080410a7c69a27a95d715ca4
wellsejoshua <84493818+wellsejoshua@users.noreply.github.com>
2022-03-22 00:34:19 -0500

    Added DataUtility class

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
+using AddressBookMVC.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,9 +14,22 @@ namespace AddressBookMVC
 {
   public class Program
   {
-    public static void Main(string[] args)
+    //public static void Main(string[] args)
+    //{
+    //  CreateHostBuilder(args).Build().Run();
+    //}
+
+    public static async Task Main(string[] args)
     {
-      CreateHostBuilder(args).Build().Run();
+      var host = CreateHostBuilder(args).Build();
+
+      var dbContext = host.Services
+                          .CreateScope().ServiceProvider
+                          .GetRequiredService<ApplicationDbContext>();
+
+      await dbContext.Database.MigrateAsync();
+
+      host.Run();
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
